d6a59e133d4a90bae14c9876b7a78f487f776dd3
Tildozer <actninswitch@gmail.com>
2023-12-06 21:49:59 -0700

    started implementing user settings into dark mode, just need to change logic so it doesnt force you into one mode

--- src/components/theme/ThemeSlider.tsx
+++ src/components/theme/ThemeSlider.tsx
@@ -1,15 +1,33 @@
-import React, { ReactNode, useState } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 
 type Props = {
   children: ReactNode;
 };
 
+declare global {
+  interface Window {
+    darkMode: boolean | undefined;
+  }
+}
+
 const ThemeSlider = ({ children }: Props) => {
-  const [darkMode, setDarkMode] = useState(true);
+  const [darkMode, setDarkMode] = useState(false);
 
   const toggleTheme = () => {
     setDarkMode(!darkMode);
+    window.darkMode = darkMode;
   };
+
+  useEffect(() => {
+    if (!window.darkMode) {
+      if (window.darkMode === null && window.matchMedia("(prefers-color-scheme: dark)")) {
+        setDarkMode(true);
+      }
+      window.darkMode = darkMode;
+    } else {
+      setDarkMode(window.darkMode);
+    }
+  }, []);
 
   return (
     <div>

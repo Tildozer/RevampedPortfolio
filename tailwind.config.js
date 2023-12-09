d2c5a81f70a32f313b4bbff5c788a73e361fd2d2
Tildozer <actninswitch@gmail.com>
2023-12-09 13:40:43 -0700

    started nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        'Bebas' : ['Bebas Neue', 'verdana']
+      }
     },
   },
   plugins: [],
